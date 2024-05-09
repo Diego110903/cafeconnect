e32272dbedbdff0527ee4dd2b4abeda505687c02
usuario@usuario-pc <damg1312@uotlook.es>
2024-05-09 10:46:41 -0500

    "2 commit"

--- js/script.js
+++ js/script.js
@@ -5,14 +5,22 @@ function ruta(url){
     location.href=url
 }
 
+$("#btn-iniciar").on("click",()=>{
+    $("#form_login").submit()
+})
+
     $("#btnnuevo").on("click",()=>{
     ruta("OlvidotuContrasena.html")
     })
 
     $("#form_login").on("submit",()=>{
-
-    })
-
+       // alert("ON submit del formulario");
+       let data = $("#form_login").serialize()
+        alert("Datos envia/n/n"+data)
+        ruta("../control/login.php")
+        return false
+        //ajax
+})
     //$(".botoninicio").hide()
 
     
