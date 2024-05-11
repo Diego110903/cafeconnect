0111e54949f4849b80a0065cec7878d75d04c8e2
usuario@usuario-pc <damg1312@uotlook.es>
2024-05-10 19:08:13 -0500

    commit aplicando tecnica de ajax y metodo de fetch y algo de json

--- control/login.php
+++ control/login.php
@@ -1,5 +1,5 @@
 <?php
         echo "estas en control de login <br/>";
         //echo $_POST["correo"]."  ".$_POST["contrasena"]
-        echo $_GET["correo"]."  ".$_GET["contrasena"]
+        echo $_GET["user"]."  ".$_GET["pass"]
 ?>
--- js/main.js
+++ js/main.js
@@ -6,19 +6,60 @@ function ruta(url){
 }
 
 $("#btn-iniciar").on("click",()=>{
-    $("#form_login").submit()
+    //$("#form_login").submit()
+
 })
 
     $("#btnnuevo").on("click",()=>{
     ruta("OlvidotuContrasena.html")
     })
 
-    $("#form_login").on("submit",()=>{
+    $("#form_login").on("submit", (e)=>{
+    e.preventDefault();
        // alert("ON submit del formulario");
-       let data = $("#form_login").serialize()
-        alert("Datos envia/n/n"+data)
-        ruta("../control/login.php")
-        return false
+        let nom_usu=$("#correo").val()
+        let pass_usu=$("#contrasena").val()
+        // let data = $("#form_login").serialize()
+        if(nom_usu!="" && pass_usu!= ""){
+            let info = {
+                "user":nom_usu,
+                "pass":pass_usu
+            }
+            alert("ok --- inicia ajax")
+            // aplicando tecnica ajax
+            $.ajax({
+                data: info,
+                url: "../control/login.php?",
+                type: "GET",
+                beforeSend:()=>{
+                    console.log("procesando la peticion...")
+                }
+            }).done((resp)=>{
+                $("#div-msg1").html(resp)
+            })
+            // fin ajax
+
+            // aplicando el metodo de fetch
+            const div = document.getElementById("div-msg2")
+            fetch("../control/login.php?user="+nom_usu+"&pass="+pass_usu,info).then((resp)=>{
+                div.innerHTML += resp.text()})
+            // fin fetch
+            // fetch("../control/login.php",info).then((resp)=>resp.jason()).then((dataj)=>{})
+
+
+            // alert("Datos envia\n\n"+data)
+            // return false
+    }else{
+        alert("ingrese la informacion requerida")
+        //alert("Datos envia\n\nUsuario: "+nom_usu+"\ncontasena: "+pass_usu)
+        $("#correo").focus()
+        
+        }
+            
+        // alert("Datos envia\n\nUsuario: "+nom_usu+"\ncontasena: "+pass_usu)
+        // ruta("../control/login.php")
+        //alert("ok --- inicia ajax")
+        
         //ajax
 })
 
@@ -31,14 +72,14 @@ $(()=>{
    $("#btnnuevo").html("olvido tu contrasena").css({'color':'black', 'font-size':'1.3em'})
 //$("#btn-iniciar").remove().fadeOut(3000)animacion
     //$("#btn-iniciar").slideUp(300).delay(800).fadeIn(400);animacion
-    $(".logo-container").hide(4000).delay(1000).show(2000)
+    $(".logo-container").hide(2000).delay(1000).show(2000)
     //$(".logo-container").slideUp(300).delay(800).fadeIn(400);animacion
     //$(".logo-container").hide(3000)
 })
 
 
-
-
+ 
+//USER="+nom_usu+"&"+pass_usu
 
     //$(".botoninicio").hide()
 
