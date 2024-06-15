0e10c9ca009ca9ae2254fc9ecd8ae2be72e9cd60
usuario@usuario-pc <damg1312@uotlook.es>
2024-06-14 21:22:26 -0500

    commit se agrego un nuevo archivo llamado menu principal html y css y se modifico login.php login.js iniciar sesion html

--- control/login.php
+++ control/login.php
@@ -1,11 +1,40 @@
 <?php
-/*if($_SERVER["REQUEST_METHOD"]=="post"){
-        if($_POST["user"]!="" && $_POST["clave"]!=""){
-                // echo $_post["user"];
-        }
-        header("HTTP/1.1 200 ok");
-}*/        
+require_once("config.class.php")
+//header('content-type: application/json');
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    try {
+        $_post = json_decode(file_get_contents('php://input'),true);
+        //var_dump($_post);
+        if($_post["user"]!="" && $_post["pass"]!=""){
+        //      echo $_post["user"];
+                $bd = new Config();
+                $conn = $bd->conexion();
+                $sql ="INSERT INTO tbusuario(IdUsuarioPK, IdRolFK, UsuNombre, UsuEmail, UsuContraseña) VALUES ()";
+                $stmt = $conn ->prepare($sql);
+                if($stmt->execute()){
+                echo "SQL realizada correctamente";
+                }else{
+                echo "SQL no aplicada";
+                }
 
+
+
+
+            header("HTTP/1.1 200");
+            echo json_encode(['code'=>200,'msg' => 'Consulta finalizada correctamente']);
+        //     http_response_code(200);    
+        }
+        exit();
+    } catch (Exception $ex) {
+        header("HTTP/1.1 500");
+        echo json_encode(['code'=>500,'msg' => 'Error, '.$ex->getMessage()]);
+    }  
+}else {
+    header("HTTP/1.1 400");
+    echo json_encode(['code'=>400, 'msg' => 'Error, la peticion no se pudo procesar']);    
+}               
+
+?>
 
 
 
--- js/login.js
+++ js/login.js
@@ -1,38 +1,47 @@
+let divMsg = document.querySelector("#msgLogin")
 
+const enviarLogin = async (url="", method = "", param = undefined)=>{
+    // if(param !== undefined && method==="GET") url += "?"+ new URLSearchParams(param)
+    // if (method === "GET") method={method,headers: {'content-type':'application/json'}}
+    if (method === "POST") method={method,headers: {'content-type':'application/json'},body: JSON.stringify(param)}
+    // if (method === "PUT") method={method,headers: {'content-type':'application/json'},body: JSON.stringify(param)}
+    // if (method === "DELETE") method={method,headers: {'content-type':'application/json'},body: JSON.stringify(param)}
+
+
+    divMsg.innerHTML = `<div class="spinner-border" role="status"><span class="sr-only">Loading...</span></div>`
+    fetch(url,method)
+         .then(response =>response.json())
+         .then(data=> validarLogin(data))
+         .catch(e => console.log(e))
+    }
+    const validarLogin = (info)=>{
+        (info.code===200) ? window.location.href="menuprincipal.html" : divMsg.innerHTML =`<b class='text-danger'>${info.msg}</b>`
+        
+           
+    }
 
     let form = document.getElementById("form_login")
 
     form.addEventListener('submit', (e)=>{
         e.preventDefault()
-        // alert("estoy aqui... ok")
-
         let user = document.getElementById("user").value
-        let pass = document.getElementById("pass").value
-        let var_json = JSON.stringify({usu: user,clave: pass})
-        //  console.log(json)
-        //  console.log(JSON.parse(json))
-
-
+        let pass = md5(document.getElementById("pass").value)
+        let param = {user, pass}
+        // console.log(param)
+        // console.log(JSON.stringify(param))
+       
         if(user!=="" && pass!==""){
-             //  alert("estoy aqui... ok")
-            let info = {
-                method: "POST",
-                headers: {'content-type':'application/json'},
-                body: var_json
-           
-            }
-            fetch("../control/login.php",info)
-            // .then((resp)=>{div.innerHTML += resp.json()})
-    
+            let method = "POST"
+            // fetch('../control/login.php', {method:'POST' ,body: JSON.stringify(param)})
+            // fetch('../control/login.php?'+ new URLSearchParams(param), {method: 'GET'})
+            enviarLogin("../control/login.php",method,param)
+            
         }
-        /*if(e.target === $btniniciar){
-            if($pass.value !== "" && $user.value !==""){
-                e.preventDefault();
-                window.location.href="menu.html"
-            }
-        }*/
-        return false    
     })
+
+    
+
+
 
 
 
